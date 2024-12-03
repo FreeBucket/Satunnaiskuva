c007401a27aa593df2c0135d35bcff9066f9ed11
SeYksTyyppiSielJossakin <jakehirvi27@gmail.com>
2024-12-03 12:35:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const images = ["kuva1.jpg", "kuva2.jpg", "kuva3.jpg"];
+
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+}
+
+document.querySelector("button").addEventListener("click", () => {
+    const imageElement = document.querySelector("img");
+    const newImage = getRandomImage();
+    imageElement.src = newImage;
+});
 
